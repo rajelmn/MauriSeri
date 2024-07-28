8f46b0100d6f5f301838dc6c156a7deeaec60cfe
brahim <139687519+rajelmn@users.noreply.github.com>
2024-07-28 19:14:04 +0000

    Update server.js

--- mauri Seri/server/server.js
+++ mauri Seri/server/server.js
@@ -1,1 +1,19 @@
+const express = require('express');
+const fs = require('node:fs');
+const cors = require('CORS')
+const app = express();
+const port = 3000;
 
+app.use(cors())
+app.use(express.text())
+
+app.post('/', (req, res) => {
+  
+ fs.appendFile('server/user_information.txt', `${req.body}, `, err => {
+  if(err) throw new Error
+ })
+})
+
+app.listen(port, () => {
+  console.log('app is running in port' + "" + port);
+})
